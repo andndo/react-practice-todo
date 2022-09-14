da5364a39ecc64a6c0dfa3f8dd008afdd9d1c1b8
anda <ando6160@gmail.com>
2022-09-14 20:15:24 +0900

    3rd

--- src/App.js
+++ src/App.js
@@ -4,26 +4,30 @@ import * as S from "./style";
 function App() {
   const [indexs, setIndexs] = useState([]);
   const [todo, setTodo] = useState("");
+  const [fix, setFix] = useState(false);
+
   const onChange = (e) => {
     setTodo(e.target.value);
   };
   const onRemove = (e) => {
-    console.log(e);
     const data = indexs.filter((data) => data.id !== e);
-    console.log(data);
     setIndexs(data);
   };
   const nextId = useRef(1);
   const onReset = () => {
-    const data = {
-      id: nextId.current,
-      name: todo,
-    };
-    setIndexs([...indexs, data]);
-    setTodo("");
-    nextId.current += 1;
+    if (todo !== "") {
+      const data = {
+        id: nextId.current,
+        name: todo,
+      };
+      setIndexs([...indexs, data]);
+      setTodo("");
+      nextId.current += 1;
+    }
   };
-
+  const onFix = () => {
+    setFix(!fix);
+  };
   const onKeyPress = (e) => {
     if (e.key == "Enter") {
       onReset();
@@ -32,9 +36,18 @@ function App() {
   const Main = (props) => {
     return (
       <S.List>
-        <h3>{props.item.name}</h3>
-        <button>수정</button>
-        <button onClick={() => onRemove(props.item.id)}>X</button>
+        {fix != false ? (
+          <>
+            <input onChange={onChange} value={todo} placeholder="입력"/>
+            <button onClick={onFix}>확인</button>
+          </>
+        ) : (
+          <>
+            <h3>{props.item.name}</h3>
+            <button onClick={onFix}>수정</button>
+            <button onClick={() => onRemove(props.item.id)}>X</button>
+          </>
+        )}
       </S.List>
     );
   };
@@ -51,7 +64,6 @@ function App() {
         onKeyDown={onKeyPress}
       />
       <button onClick={onReset}>입력</button>
-      <button onClick={asdf}></button>
       <div>{nameList}</div>
     </>
   );
