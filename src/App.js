ce7859fc68b18d850bc5a75b37f1ca40c792952a
anda <ando6160@gmail.com>
2022-09-14 19:10:34 +0900

    2th

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import * as S from "./style";
 
 function App() {
@@ -7,10 +7,23 @@ function App() {
   const onChange = (e) => {
     setTodo(e.target.value);
   };
-  const onReset = (e) => {
-    setIndexs(indexs.concat(todo));
-    setTodo("");
+  const onRemove = (e) => {
+    console.log(e);
+    const data = indexs.filter((data) => data.id !== e);
+    console.log(data);
+    setIndexs(data);
   };
+  const nextId = useRef(1);
+  const onReset = () => {
+    const data = {
+      id: nextId.current,
+      name: todo,
+    };
+    setIndexs([...indexs, data]);
+    setTodo("");
+    nextId.current += 1;
+  };
+
   const onKeyPress = (e) => {
     if (e.key == "Enter") {
       onReset();
@@ -19,12 +32,16 @@ function App() {
   const Main = (props) => {
     return (
       <S.List>
-        <h3>{props.name}</h3>
-        <button>X</button>
+        <h3>{props.item.name}</h3>
+        <button>수정</button>
+        <button onClick={() => onRemove(props.item.id)}>X</button>
       </S.List>
     );
   };
-  const nameList = indexs.map((index) => <Main name={index} />);
+  function asdf() {
+    console.log(indexs);
+  }
+  const nameList = indexs.map((item) => <Main item={item} />);
   return (
     <>
       <input
@@ -34,6 +51,7 @@ function App() {
         onKeyDown={onKeyPress}
       />
       <button onClick={onReset}>입력</button>
+      <button onClick={asdf}></button>
       <div>{nameList}</div>
     </>
   );
